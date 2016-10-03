5c069edd11f0971ba26fe95fa3e9009e89c4d8bc
Jermaine Forbes <jermaine0forbes@gmail.com>
2016-10-02 23:35:11 -0400

    Another attempt at making express work

--- app.js
+++ app.js
@@ -1,27 +1,16 @@
 var express  = require('express');
 var app = express();
 var path = require("path");
-var hbs = require("express-handlebars");
-var text = ` This is some regular text in a paragraph`;
-app.set('port', process.env.PORT || 3000);
-app.engine('handlebars', hbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-app.set("views", __dirname+"/views");
-app.use(express.static(__dirname+"/public"));
+var hbs = require('express-handlebars');
+var port = process.env.PORT || 3000;
+app.engine(hbs.engine)
+app.set('views', hbs.create{defaultLayout: 'main'});
+app.set('view engine',"handlebars")
+
+app.use(express.static(path.join(__dirname,'public'))
+
 
 app.get("/", function(req,res){
-  res.render('home',{text:text})
-});
-
-app.use(function(req,res,next){
-  res.status(404);
-  res.render('404')
-});
-
-app.use(function(req,res){
-  res.status(500);
-  res.render('500')
-})
-app.listen(app.get('port'), function(){
-  console.log(`Server has been connected to port ${app.get("port")}`);
+  res.type = "text/html";
+  res.send("This is a response");
 })
