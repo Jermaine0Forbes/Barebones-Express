e85e4c19511bb11d21338e2f4b32b8ecb9e933c9
Jermaine Forbes <jermaine0forbes@gmail.com>
2016-10-03 00:32:27 -0400

    Fresh slate

--- app.js
+++ app.js
@@ -1,25 +1,3 @@
 var express  = require('express');
 var app = express();
 var path = require("path");
-var hbs = require('express-handlebars');
-var port = process.env.PORT || 3000;
-var err = `this is an error`;
-var routes = require("./app_server/routes/index.js")
-
-app.engine( 'handlebars',hbs({defaultLayout: 'main'}));
-app.set('views', __dirname+"/views");
-app.set('view engine',"handlebars")
-
-app.use(express.static(path.join(__dirname,'public')));
-
-
-app.use("/", routes);
-
-app.use(function(req,res){
-res.status(404);
-res.render('404', {text: err});
-
-})
-app.listen(port, function(req,res){
-  console.log(`Server has been connected to port ${port}`);
-})
--- app_server/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var pages = require('../controller/routes.js')
-
-router.get("/", pages.home);
-router.get("/about", pages.about);
-router.get("/contact", pages.contact);
-
-module.exports = router;
